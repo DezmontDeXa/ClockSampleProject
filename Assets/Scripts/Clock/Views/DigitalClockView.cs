dd7b74c82dda8e9dbaf7459eca717983bcea0401
DezmontDeXa <flatron2800@gmail.com>
2023-07-11 19:48:07 +0300

    Create DiditalClockView

---
+++ Assets/Scripts/Clock/Views/DigitalClockView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace DDX.Clock.Views
+{
+    public class DigitalClockView : MonoBehaviour, ITimeView
+    {
+        [SerializeField] private TMP_Text _timeText;
+        public void SetTime(TimeSpan time)
+        {
+            _timeText.text = time.ToString("hh\\:mm\\:ss");
+        }
+    }
+}
