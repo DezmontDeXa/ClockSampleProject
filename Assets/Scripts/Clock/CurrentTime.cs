39515d855016e3d19675b587fd0354db2bdef8fa
DezmontDeXa <flatron2800@gmail.com>
2023-07-11 18:00:40 +0300

    Add CurrentTime

---
+++ Assets/Scripts/Clock/CurrentTime.cs
@@ -0,0 +1,11 @@
+using DDX.Clock.TimeProviders;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentTime : MonoBehaviour
+{
+    private List<ITimeProvider> _timeProviders;
+
+
+}
