c9f5ecf93b7db8900b48509cc4d6b71372019dc3
DezmontDeXa <flatron2800@gmail.com>
2023-07-11 19:30:26 +0300

    Split CurrentTime to CurrentTime and webTime

--- Assets/Scripts/Clock/CurrentTime.cs
+++ Assets/Scripts/Clock/CurrentTime.cs
@@ -1,11 +1,44 @@
-using DDX.Clock.TimeProviders;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
+using System;
 
-public class CurrentTime : MonoBehaviour
+namespace DDX.Clock
 {
-    private List<ITimeProvider> _timeProviders;
+    public class CurrentTime : MonoBehaviour
+    {
+        private WebTime _webTime;
 
+        private float _lastUpdateUnityTime = 0;
+        private TimeSpan _lastUpdateTime = TimeSpan.Zero;
 
+        public TimeSpan Time { get; private set; }
+
+        public event EventHandler<TimeSpan> TimeChanged;
+
+        [Inject]
+        private void Init(WebTime webTime)
+        {
+            _webTime = webTime;
+            _webTime.WebTimeUpdated += WebTimeUpdated;
+        }
+
+        private void WebTimeUpdated(object sender, TimeSpan e)
+        {
+            Time = e;
+            _lastUpdateTime = e;
+            _lastUpdateUnityTime = UnityEngine.Time.realtimeSinceStartup;
+        }
+
+        private void Update()
+        {
+            Time = GetCurrentTime();
+            TimeChanged?.Invoke(this, Time);
+        }
+
+        private TimeSpan GetCurrentTime()
+        {
+            return _lastUpdateTime + new TimeSpan(0, 0, (int)(UnityEngine.Time.realtimeSinceStartup - _lastUpdateUnityTime));
+        }
+
+    }
 }
---
+++ Assets/Scripts/Clock/WebTime.cs
@@ -0,0 +1,69 @@
+﻿using DDX.Clock.TimeProviders;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+using System.Linq;
+using System;
+using Cysharp.Threading.Tasks;
+using System.Threading;
+
+namespace DDX.Clock
+{
+    public class WebTime : IDisposable
+    {
+        private TimeSpan _fixRate = new TimeSpan(1, 0, 0);
+        private List<ITimeProvider> _timeProviders;
+        private TimeSpan _lastWebTime;
+        private CancellationTokenSource _cts = new CancellationTokenSource();
+
+        public TimeSpan LastWebTime => _lastWebTime;
+
+        public event EventHandler<TimeSpan> WebTimeUpdated;
+
+
+        [Inject]
+        public void Init(IEnumerable<ITimeProvider> timeProviders)
+        {
+            _timeProviders = timeProviders.ToList();
+            UpdateCurrentTimeAsync();
+            UniTask.RunOnThreadPool(FixTime);
+        }
+
+        private async void FixTime()
+        {
+            while (!_cts.IsCancellationRequested)
+            {
+                await UniTask.Delay(_fixRate);
+                await UpdateCurrentTimeAsync();
+            }
+        }
+
+        private async UniTask UpdateCurrentTimeAsync()
+        {
+            foreach (var provider in _timeProviders.ToList())
+            {
+                try
+                {
+                    _lastWebTime = await provider.GetTimeAsync();
+                    WebTimeUpdated.Invoke(this, _lastWebTime);
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    _timeProviders.Remove(provider);
+                    if (_timeProviders.Count > 0)
+                        Debug.Log(ex.Message);
+                    else
+                        Debug.LogError("All providers not available");
+                    continue;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _cts.Cancel();
+        }
+
+    }
+}
