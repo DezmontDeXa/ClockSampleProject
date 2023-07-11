8639723461f31b12c551cf07f6dcd8b7340c7a43
DezmontDeXa <flatron2800@gmail.com>
2023-07-11 22:46:34 +0300

    Init WallClockAlarmInput

---
+++ Assets/Scripts/Clock/Alarm/WallClockAlarmInput.cs
@@ -0,0 +1,21 @@
+﻿using DDX.Clock.Views;
+using System;
+using UnityEngine;
+
+namespace DDX.Clock.Alarm.Inputs
+{
+    public class WallClockAlarmInput : MonoBehaviour, IAlarmInput
+    {
+        [SerializeField] private WallClockView _alarmWallClockView;
+
+        public event EventHandler<TimeSpan?> AlarmValueChanged;
+
+        public void SetAlarmValue(TimeSpan? time)
+        {
+            if (time == null)
+                time = new TimeSpan();
+
+            _alarmWallClockView.SetTime(time.Value);
+        }
+    }
+}
