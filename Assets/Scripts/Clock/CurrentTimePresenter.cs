87154554533902611462bd0507ae3af74b7fd113
DezmontDeXa <flatron2800@gmail.com>
2023-07-11 19:30:43 +0300

    Create CurrentTimePresenter

---
+++ Assets/Scripts/Clock/CurrentTimePresenter.cs
@@ -0,0 +1,41 @@
+﻿using DDX.Clock.Views;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace DDX.Clock
+{
+    public class CurrentTimePresenter : MonoBehaviour
+    {
+        private CurrentTime _currentTime;
+        private IEnumerable<ITimeView> _views;
+
+        [Inject]
+        public void SetCurrentTime(CurrentTime currentTime)
+        {
+            _currentTime = currentTime;
+            if (_views != null)
+                Init();
+        }
+
+        [Inject]
+        public void SetViews(IEnumerable<ITimeView> views)
+        {
+            _views = views;
+            if (_currentTime != null)
+                Init();
+        }
+
+        private void Init()
+        {
+            _currentTime.TimeChanged += (s, e) => TimeChanged(e, _views);
+        }
+
+        private void TimeChanged(TimeSpan time, IEnumerable<ITimeView> views)
+        {
+            foreach (var view in views)
+                view.SetTime(time);
+        }
+    }
+}
